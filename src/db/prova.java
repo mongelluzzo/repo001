48fcd726235c41bbb9aec9f7c16190ca3acfb0e6
rober <rober@192.168.1.3>
2021-07-21 15:23:20 +0200

    First Load

---
+++ src/db/prova.java
@@ -0,0 +1,16 @@
+package db;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class prova {
+private static final Logger Log = LogManager.getLogger(prova.class);
+public static void main (String[] args ) {
+	Log.debug ("1, 2,3");
+}
+
+}
+
+
+
+
+
+
